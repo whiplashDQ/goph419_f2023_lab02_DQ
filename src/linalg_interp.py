5bf988c855ac3d70dbe378021cad3ae9a1409704
whiplashDQ <55807704+whiplashDQ@users.noreply.github.com>
2023-11-20 08:31:56 +0000

    11

--- src/linalg_interp.py
+++ src/linalg_interp.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d, UnivariateSpline
 
 
 def gauss_iter_solve(A,b,tol,alg):
@@ -52,4 +53,44 @@ def gauss_iter_solve(A,b,tol,alg):
         return None
 
 def spline_function(xd,yd,order):
-       
+    """
+    Compute the spline function for a given set of data points
+    xd: float array, increasing values of x 
+    yd: float array, corresponding values of y = f(x)
+    order: order of the spline function (1, 2, 3) default is 3
+    """
+    
+    import numpy as np
+    from scipy.interpolate import interp1d, UnivariateSpline
+
+    def spline_function(xd, yd, order):
+        # Validation checks
+        # Converting to numpy arrays
+        xd = np.asarray(xd).flatten()
+        yd = np.asarray(yd).flatten()
+
+        if xd.size != yd.size:
+            raise ValueError("xd and yd must have the same length.")
+        if len(np.unique(xd)) != xd.size:
+            raise ValueError("xd contains repeated values.")
+        if not np.array_equal(xd, np.sort(xd)):
+            raise ValueError("xd values must be in increasing order.")
+        if order not in [1, 2, 3]:
+            raise ValueError("order must be 1, 2, or 3.")
+
+        # Creating the spline
+        if order == 1:
+            spline = interp1d(xd, yd, kind='linear')
+        elif order == 2:
+            spline = interp1d(xd, yd, kind='quadratic')
+        elif order == 3:
+            spline = interp1d(xd, yd, kind='cubic' )
+        
+        # Check if input is outside the range of xd values
+        if np.any(xd[0] > xd):
+            raise ValueError("x is outside the range of xd values.", xd,'is less than', xd[0])
+        if np.any(xd > xd[-1]):
+            raise ValueError("x is outside the range of xd values.", xd,'is greater than', xd[-1])    
+        
+        return spline
+    
